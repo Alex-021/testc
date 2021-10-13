6591729f37ea37962302dd328f1b27e4c1385d23
Alex-021 <92319992+Alex-021@users.noreply.github.com>
2021-10-13 05:53:16 +0330

    Update func.inc.php

--- func.inc.php
+++ func.inc.php
@@ -1,5 +1,5 @@
 <?php
 function sendMessage($chat_id, $message, $keyboard) {
-    $url = "https://api.telegram.org/bot2051670997:AAFfQ1soA-qMlJNGclkeTpRayA2Ni8pu2vI/sendMessage?chat_id=$chat_id&text=$message&parse_mode=Markdown&reply_markup=$keyboard";
+    $url = "https://api.telegram.org/bot2081391287:AAHhRxBdd7aLNM2L3-M1gAVSZgcg3Od0_Y0/sendMessage?chat_id=$chat_id&text=$message&parse_mode=Markdown&reply_markup=$keyboard";
     file_get_contents($url);
 }
