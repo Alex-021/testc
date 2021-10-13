97014d7dc13f54a222ba8b12e905e1b3c8e2df1c
Alex-021 <92319992+Alex-021@users.noreply.github.com>
2021-10-13 06:04:03 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-function sendMessage($chat_id, $message) {
-    $url = "https://api.telegram.org/bot2081391287:AAHhRxBdd7aLNM2L3-M1gAVSZgcg3Od0_Y0/sendMessage?chat_id=$chat_id&text=$message&parse_mode=Markdown&reply_markup=$keyboard";
-    file_get_contents($url);
-}
 // require_once './func.inc.php';
 $update = file_get_contents('php://input');
 // file_put_contents('bot.txt',$update);
